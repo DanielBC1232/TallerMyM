8a91baf18f16a43dc2c2aee2c0aa1b08663e68ad
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-01-25 09:25:29 -0600

    estilos personalizados scss y boostrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
+import './styles/custom.scss';
+import './styles/style.css'
 
 export function App() {
   return (
     <article className='md-followCard'>
       <header>
         <img alt="Avatar" src="https://img.icons8.com/color/128/vite.png" />
-        <div>
+        <div className={''}>
           <strong>Nombre Apellido Apellido</strong>
           <span>@usuario</span>
         </div>
       </header>
       <aside>
-        <button>
+        <button className={'btn btn-primary'}>
             Seguir
         </button>
       </aside>
