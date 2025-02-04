a1d9afca5ecc774492b91e110ca3312eedad37ec
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-02-03 21:59:00 -0600

    React-router-dom (Redireccionamiento)
    ajustes de redireccionamiento y fix de componentes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,45 @@
+import "rsuite/dist/rsuite.min.css";
+import "./styles/custom.css";
+import "./styles/style.css";
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Router, Route, Routes } from "react-router-dom";
 
 import Detalles from "./features/inventario/pages/Detalles";
-import IndexInventario from "./features/inventario/pages";
+import IndexInventario from "./features/inventario/pages/Index";
 import Editar from "./features/inventario/pages/Editar";
 import Agregar from "./features/inventario/pages/Agregar";
-import CrearPerfil from "./features/perfil/pages/CrearPerfil"
+import CrearPerfil from "./features/perfil/pages/CrearPerfil";
 
-import "rsuite/dist/rsuite.min.css";
 
-import "./styles/custom.css";
-import "./styles/style.css";
+
+
 
 const App = () => {
   return (
-    
     <div className="app">
       <Header />
+
       <main style={{ minHeight: "95vh" }}>
         {/*
-        <IndexInventario />
-        <Detalles />
-        <Agregar/>
+           <IndexInventario />
+          <Detalles /> 
+          <Agregar /> */}
 
-        */}
-        <CrearPerfil/>
+        <Routes>
+          {/*Rutas inventario */}
+          <Route path="/" element={<App />} />
+          <Route path="/agregar" element={<Agregar />} />
+          <Route path="/inventario" element={<IndexInventario />} />
 
+          {/*  */}
+          <Route path="/perfil" element={<CrearPerfil />} />
 
+        </Routes>
+      
       </main>
+
       <Footer />
     </div>
   );
