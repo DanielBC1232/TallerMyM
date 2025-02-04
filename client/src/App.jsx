dbdc9e4b77baa6d0b3198c3fe092fb66cfaf8c23
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-02-04 16:50:14 -0600

    Correccion routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import "./styles/style.css";
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Detalles from "./features/inventario/pages/Detalles";
 import IndexInventario from "./features/inventario/pages/Index";
@@ -12,32 +12,31 @@ import Editar from "./features/inventario/pages/Editar";
 import Agregar from "./features/inventario/pages/Agregar";
 import CrearPerfil from "./features/perfil/pages/CrearPerfil";
 
-
-
-
-
 const App = () => {
   return (
     <div className="app">
       <Header />
 
       <main style={{ minHeight: "95vh" }}>
-        {/*
-           <IndexInventario />
-          <Detalles /> 
-          <Agregar /> */}
+
+          {/* ******************
+          
+          Usar el url:
+          /[modulo]-acción 
+
+          ****************** */}
+
 
         <Routes>
-          {/*Rutas inventario */}
-          <Route path="/" element={<App />} />
-          <Route path="/agregar" element={<Agregar />} />
+          <Route path="/" element={<IndexInventario />} />
+
+          {/* Rutas para inventario */}
+          <Route path="/inventario-agregar" element={<Agregar />} />
           <Route path="/inventario" element={<IndexInventario />} />
 
-          {/*  */}
-          <Route path="/perfil" element={<CrearPerfil />} />
-
+          {/* Ruta para perfil */}
+          <Route path="/perfil-crear" element={<CrearPerfil />} />
         </Routes>
-      
       </main>
 
       <Footer />
