2aa36f5f3b7a3128fefa3baa8ec344680a032bfe
Paola Segura <145728086+Paobella@users.noreply.github.com>
2025-02-06 11:18:56 -0600

    verificar correo

--- client/src/features/perfil/pages/CrearPerfil.jsx
+++ client/src/features/perfil/pages/CrearPerfil.jsx
@@ -8,7 +8,7 @@ const CrearPerfil = () => {
       return;
     }
     try {
-      const response = await fetch("http://localhost:3000/api/email/send-email", {
+      const response = await fetch("http://127.0.0.1:3000/api/email/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email : data.email}),
