dadbb980993919620a26c8394816a9a9e58f5729
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-02-06 11:22:13 -0600

    app.js

--- server/dist/services/app.js
+++ server/dist/services/app.js
@@ -1,34 +1,39 @@
 "use strict";
-const express = require('express');
-const usuarioRoutes = require('../routes/perfil/usuarioRoutes');
-const emailRoutes = require("../routes/perfil/emailRoutes");
-
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-//import categoriaRoutes from './routes/inventario/categoriaRoutes';
-require("../config/database");
+const cors_1 = __importDefault(require("cors"));
+const categoriaRoutes_1 = __importDefault(require("../routes/inventario/categoriaRoutes"));
+const marcaRoutes_1 = __importDefault(require("../routes/inventario/marcaRoutes"));
+const proveedorRoute_1 = __importDefault(require("../routes/inventario/proveedorRoute"));
+const vehiculosCompatiblesRoutes_1 = __importDefault(require("../routes/inventario/vehiculosCompatiblesRoutes"));
+const productoRoutes_1 = __importDefault(require("../routes/inventario/productoRoutes"));
+const database_1 = require("../config/database");
 const app = (0, express_1.default)();
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-}));
-// Middleware
-app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/ping', (_req, res) => {
-    console.log("Ping");
-    res.send("respuesta ping Awa");
-});
-//Ruta API
-app.use('/api/usuarios', usuarioRoutes); 
-app.use('/api/email', emailRoutes);
-//app.use('/api/categorias', categoriaRoutes);
-// Iniciar servidor
+app.use(express_1.default.json()); // Middleware
+const PORT = 3000; // Iniciar servidor
 app.listen(3000, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+}));
+(0, database_1.connectDB)() // conexion BD
+    .then(() => console.log("Conectado a la base de datos"))
+    .catch((err) => {
+    if (err instanceof Error) {
+        console.error("Error en la conexión:", err.message);
+    }
+    else {
+        console.error("Error en la conexión:", err);
+    }
+});
+//* Rutas Inventario
+app.use("/categorias", categoriaRoutes_1.default);
+app.use("/marcas", marcaRoutes_1.default);
+app.use("/proveedor", proveedorRoute_1.default);
+app.use("/vehiculos-compatibles", vehiculosCompatiblesRoutes_1.default);
+app.use("/productos", productoRoutes_1.default);
+//* Rutas Inventario
