d33a9ede67b75f8b888ba4fa8d260405a0e62852
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-04-23 16:01:30 -0600

    fix img

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = ({ children }) => {
       <nav id="sidebar" className="sidebar shadow-sm">
         <div className="sidebar-content">
           <Link className="sidebar-brand" to="/flujo">
-            <img src="./../public/favicon.png" alt="Descripción del logo" height="60"/>Taller MyM
+            Taller MyM
           </Link>
           {/* icono *** */}
           <hr className="text-primary mx-3 m-0"></hr>
